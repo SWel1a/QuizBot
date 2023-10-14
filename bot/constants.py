2355a1894b8c610d0c14871bf550629f048a8ab5
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-10-14 19:06:57 +0200

    Create constants.py
    Default values provided by SWel1a
    
    Co-Authored-By: Dmitry Wolf <146629385+SWel1a@users.noreply.github.com>
    Co-Authored-By: Anton Potapov <47938145+flagro@users.noreply.github.com>

---
+++ bot/constants.py
@@ -0,0 +1,10 @@
+language = 'korean'  # Default language for a quiz
+
+interval_time_min = 120  # Default time
+time_units = ['s', 'm', 'h']
+default_time_unit = time_units[1]
+
+default_max_attempts = 3
+
+quiz_history_length = 100  # How many quiz history to keep in memory
+
