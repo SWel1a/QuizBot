06117164353a24d82d19dc3a6e713a32b03919ad
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-10-21 21:01:33 +0200

    new words list implementation

--- bot/words_list.py
+++ bot/words_list.py
@@ -1,7 +1,6 @@
 import json
 from utils import get_random_id
 
-
 class WordsList:
     def __init__(self, filepath):
         self.filepath = filepath
@@ -16,42 +15,40 @@ class WordsList:
 
     async def add_word(self, json_word_data):
         word_data = json.loads(json_word_data)
-        word_data["id"] = self._generate_id()
+        language = word_data["language"]
+        
         words = await self._load_words()
-        words.append(word_data)
+        if language not in words:
+            words[language] = []
+        words[language].append(word_data)
+        
         await self._save_words(words)
 
     async def remove_word(self, word_text):
-        words = self.get_words_by_text(word_text)
-        original_length = len(words)
-        if len(words) == original_length:
-            return False
+        words = await self._load_words()
+        
+        for language, lang_words in words.items():
+            words[language] = [word for word in lang_words if word['word'].lower() != word_text.lower()]
+        
         await self._save_words(words)
         return True
 
     async def get_word_by_text(self, word_text):
         words = await self._load_words()
-        for word in words:
-            if word['word'].lower() == word_text.lower():
-                return word
+        for lang_words in words.values():
+            for word in lang_words:
+                if word['word'].lower() == word_text.lower():
+                    return word
         return None
     
     async def get_words_by_text(self, word_text=None):
         words = await self._load_words()
         if word_text is not None:
-             words = [word for word in words if word['word'].lower() != word_text.lower()]
+            return {lang: [word for word in lang_words if word['word'].lower() != word_text.lower()] for lang, lang_words in words.items()}
         return words
     
     async def get_words_by_language(self, language=None):
         words = await self._load_words()
         if language is not None:
-            words = [word for word in words if word['language'].lower() == language.lower()]
+            return words.get(language.lower(), [])
         return words
-
-    async def get_languages(self):
-        words = await self._load_words()
-        languages = list(set([word['language'] for word in words]))
-        return languages
-
-    def _generate_id(self):
-        return get_random_id()
