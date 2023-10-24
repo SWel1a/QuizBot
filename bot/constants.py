8d19eb427ebc178bc2fb4846591d9661ee891135
Anton Potapov <47938145+Flagro@users.noreply.github.com>
2023-10-24 07:45:16 +0200

    default quiz type

--- bot/constants.py
+++ bot/constants.py
@@ -17,6 +17,8 @@ stopwords = {
     "korean": ["이", "그", "저", "그녀", "우리", "너", "그들", "이것", "저것", "그것", "그리고", "하지만", "또는", "입니다", "있는", "그랬어", "하나", "두", "세", "네", "다섯"]
 }
 
+DEFAULT_QUIZ_TYPE = "translate"  # Default quiz type
+
 REMAINING_ATTEMPTS_HINT = 2
 HINT_ITERATION_PERCENTAGE = 20
 
